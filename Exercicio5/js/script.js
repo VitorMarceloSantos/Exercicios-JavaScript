f8cc9545ae92faa2d86c34a27188c430ac1816bd
Vitor Marcelo <75284384+VitorMarceloSantos@users.noreply.github.com>
2022-04-25 18:15:03 -0300

    Exercicios JavaScript

--- Exercicio5/js/script.js
+++ Exercicio5/js/script.js
@@ -1,23 +1,22 @@
-/* 5 – Crie um programa que entre com os dados de altura e sexo de 5 pessoas. 
-Imprima na tela quantas pessoas são do sexo masculino e quantas pessoas são do 
-sexo feminino. Mostre também qual é a maior altura e se essa altura é de um 
-homem ou uma mulher*/
-
+/* 5 – Crie um programa que entre com os dados de altura e sexo de 5 pessoas. Imprima na tela quantas pessoas são do sexo masculino e quantas pessoas são do sexo feminino. Mostre também qual é a maior altura e se essa altura é de um homem ou uma mulher*/
 
 let arr_num = [];
 let btn_adicionar = document.querySelector("#adicionar");
 let btn_calcular = document.querySelector("#calcular");
+let homens = 0;
+let mulheres = 0;
 
 btn_adicionar.addEventListener("click",function(){
     let nome = document.querySelector("#nome_txt").value;
     let altura = Number(document.querySelector("#altura_txt").value);
-    let sexo = document.getElementsByName("sexo");
+    let sexo_inicial = document.getElementsByName("sexo"); //array de duas posições
+    let sexo_final;
     
     class Formulario{ //criando um objeto formulário, onde vai ter as informações dos participante
         constructor(nome, altura, sexo){
             this.nome = nome;
             this.altura = altura;
-            this.sexo = sexo
+            this.sexo = sexo;
         }
         set alterarNome(value){//metodos setter: alteram os valores
             this.nome = value
@@ -25,12 +24,8 @@ btn_adicionar.addEventListener("click",function(){
         set alterarAltura(value){
             this.altura = value;
         }
-        alterarSexo(sexo){
-            if(this.sexo == 0){
-                this.sexo = 1;
-            }else{
-                this.sexo = 0;
-            }
+        set alterarSexo(value){
+            this.sexo = value;
         }
         get mostrarNome(){ //métodos getter: mostram os valores
             return this.nome;
@@ -39,36 +34,32 @@ btn_adicionar.addEventListener("click",function(){
             return this.altura;
         }
         get mostrarSexo(){
-            if(sexo == 0){
-                return console.log("Masculino");
-            }else{
-                return console.log("Femino");
-            }
+            return this.sexo;
         }
     }
-    arr_num.push(new Formulario(nome, altura, sexo)); //Criando um array com os dados do formulário
+    if(sexo_inicial[0].checked){ //atribuir o sexo a cada pessoa
+        sexo_final = "Masculino";
+        homens += 1; //contador masculino
+    }else{
+        sexo_final = "Feminino";
+        mulheres += 1; //contador feminino
+    }
+    arr_num.push(new Formulario(nome, altura, sexo_final)); //Criando um array com os dados do formulário
 } ,false);
 
 btn_calcular.addEventListener("click",function(){
     let tela = document.querySelector("#resultado_tela"); 
-    let homens = 0;
-    let mulheres = 0;
     let pos_altura;
     let maior_altura = 0;
-    
-    
-    for(let i = 0; i < arr_num.length; i++){
-        if(arr_num[i].sexo[0].checked){ //Verificar a quantidade homens e mulheres
-            homens += 1;
-        }else{
-            mulheres += 1;
-        }
+
+
+    for(let i = 0; i < arr_num.length; i++){ //Faz com atribua o sexo(string) para cada um 
         if(arr_num[i].altura > maior_altura){
             maior_altura = arr_num[i].altura; //vai receber a maior altura
             pos_altura = i; //vai armazenar a posição no array da maior altura
         }
     }
-
     tela.innerHTML += ` <p>Total de Homens: ${homens} - Total de Mulheres: ${mulheres}.</p>`;
-    tela.innerHTML += `<p>A maior altura pertece a: <br>Nome: ${arr_num[pos_altura].nome} - Altura: ${maior_altura}</p>`;
+    tela.innerHTML += `<p>A maior altura pertece a: <br>Nome: ${arr_num[pos_altura].nome} - Altura: ${maior_altura}cm - Sexo: ${arr_num[pos_altura].sexo}</p>`;
+
 } ,false);
--- Exercicio7/js/script.js
+++ Exercicio7/js/script.js
@@ -4,27 +4,22 @@ let btn_adicionar = document.querySelector("#adicionar");
 
 btn_adicionar.addEventListener("click",function(){
     let data = document.querySelector("#txtnumero").value;
+    let tela = document.querySelector("#resultado_tela");
     let arr_data = [] //irá transformar a data string como data array
 
     let dataFormatada = /^[0-3]{1}[0-9]{1}[/][0-1]{1}[0-9]{1}[/][0-2]{1}[0-9]{1}[0-9]{1}[0-9]{1}/; //Expressão Regular : O ACENTO CIRCUFLEXO ELE DÁ A POSSIBILIDADE DE COLOcAR VALORES BEM ESPECIFICOS(ENTRE CHAVES NUMEROS PERMITIDOS E A BARRA, ENTRE COLCHETES A QUANTIDADE PERMITIDA)
+    
     arr_data = data.split("/");//separa os indices em 0[dia],1[mês],2[ano] 
-    
-    let dia = Number(arr_data[0].value); // dia em número
-    let mes = Number(arr_data[1].value); // mês em número
-    let ano = Number(arr_data[2].value); // ano em número
+    /*Utilizando o metodo parseInt(string, base 10) para converter a string em um número inteiro de base 10.  */
+    let dia = parseInt(arr_data[0],10); // dia em número
+    let mes = parseInt(arr_data[1],10); // mês em número
+    let ano = parseInt(arr_data[2],10); // ano em número
    
-    console.log(arr_data)
-    console.log(typeof dia)
-    console.log(dia)
-    if(dia > 31){
-        console.log("Dia invalido.")
+    let anoAtual = new Date(); //o metodo Date(.getHours()) vai passar o ano atual
+    //Validando a data
+    if((dia > 31)||(mes > 12)||((ano > anoAtual.getFullYear())||(ano < 1900))){ //Ano de nascimento tem que ser maior que 1900
+        tela.innerHTML += `<p>[ERRO] - Data Inválida!</p>`;
+    }else{
+        tela.innerHTML += `<p>Dia: ${dia} - Mês: ${mes} - Ano:${ano}</p>`;
     }
-    
-
-    
-
-    console.log(dataFormatada.test(data))
 } ,false);
-/*
-    tela.innerHTML += `<p>Maior número: ${maior}.</p><p>Menor número: ${menor}.</p>`
-*/
